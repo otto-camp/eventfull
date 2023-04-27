805c603a05a33064cbe260339979e0f35e095685
İsmail Yarar <ismailyarar4@gmail.com>
2023-04-27 23:35:17 +0300

    Add rich text editor to create better event descriptions.

---
+++ src/lib/editor/Editor.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	import { Editor } from '@tiptap/core';
+	import StarterKit from '@tiptap/starter-kit';
+	import BubbleMenu from '@tiptap/extension-bubble-menu';
+
+	let bubbleMenu: any;
+	let element: any;
+	let editor: any;
+
+	const headings = [1, 2, 3, 4, 5, 6];
+
+	onMount(() => {
+		editor = new Editor({
+			element: element,
+			extensions: [
+				StarterKit,
+				BubbleMenu.configure({
+					element: bubbleMenu
+				})
+			],
+			content: `
+        <h1>Hello Svelte! 🌍️ </h1>
+			  <p>This editor is running in Svelte.</p>
+				<p>Select some text to see the bubble menu popping up.</p>
+      `,
+			onTransaction: () => {
+				// force re-render so `editor.isActive` works as expected
+				editor = editor;
+			},
+			editorProps: {
+				attributes: {
+					class: 'p-2'
+				}
+			}
+		});
+	});
+
+	onDestroy(() => {
+		if (editor) {
+			editor.destroy();
+		}
+	});
+</script>
+
+<div
+	class="relative bg-surface-200-700-token rounded-token border border-surface-500 px-4 pb-4 pt-2 w-full max-w-4xl"
+>
+	{#if editor}
+		<div class="mb-4">
+			{#each headings as h}
+				<button
+					type="button"
+					on:click={() => editor.chain().focus().toggleHeading({ level: h }).run()}
+					class="btn"
+					class:active={editor.isActive('heading', { level: h })}
+				>
+					H{h}
+				</button>
+			{/each}
+			<button
+				type="button"
+				on:click={() => editor.chain().focus().setParagraph().run()}
+				class="btn"
+				class:active={editor.isActive('paragraph')}
+			>
+				P
+			</button>
+		</div>
+	{/if}
+
+	<div
+		class="flex p-1 rounded-token transition relative after:absolute after:inset-0 after:bg-black after:-z-10 after:rounded-token"
+		bind:this={bubbleMenu}
+	>
+		{#if editor}
+			<button
+				type="button"
+				on:click={() => editor.chain().focus().toggleBold().run()}
+				class="btn p-2 bg-black hover:bg-white/40"
+				class:active={editor.isActive('bold')}
+			>
+				Bold
+			</button>
+			<button
+				type="button"
+				on:click={() => editor.chain().focus().toggleItalic().run()}
+				class="btn p-2 bg-black hover:bg-white/40"
+				class:active={editor.isActive('italic')}
+			>
+				Italic
+			</button>
+			<button
+				type="button"
+				on:click={() => editor.chain().focus().toggleStrike().run()}
+				class="btn p-2 bg-black hover:bg-white/40"
+				class:active={editor.isActive('strike')}
+			>
+				Strike
+			</button>
+		{/if}
+	</div>
+
+	<div bind:this={element} />
+</div>
+
+<style>
+	button.active {
+		background: black;
+		color: white;
+	}
+</style>
--- src/routes/(admin)/dashboard/create-event/+page.svelte
+++ src/routes/(admin)/dashboard/create-event/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Editor from '$lib/editor/Editor.svelte';
+
 	const types = [
 		'Party',
 		'Concert',
@@ -24,7 +26,7 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section class="px-2 max-w-7xl mx-auto mt-2">
+<section class="px-2 max-w-6xl mx-auto mt-2">
 	<h1 class="text-center font-semibold text-3xl sm:text-4xl md:text-5xl lg:text-7xl">
 		Create Event
 	</h1>
@@ -34,54 +36,33 @@
 		action="/dashboard/create-event"
 		class="my-8 sm:my-12 md:my-20 flex flex-col gap-4 items-center"
 	>
-		<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-4xl w-full">
 			<div>
-				<label for="event name" class="font-medium text-base-content block">Name</label>
-				<input
-					type="text"
-					id="event name"
-					name="name"
-					placeholder="Name"
-					class="input input-bordered w-full focus-visible:outline-base-content border-base-content"
-				/>
+				<label for="event name" class="font-medium text-token">Name</label>
+				<input type="text" id="event name" name="name" placeholder="Name" class="input w-full" />
 			</div>
 			<div>
-				<label for="location" class="font-medium text-base-content block">Location</label>
+				<label for="location" class="font-medium text-token">Location</label>
 				<input
 					type="text"
 					id="location"
 					name="location"
 					placeholder="Location"
-					class="input input-bordered w-full focus-visible:outline-base-content border-base-content"
+					class="input w-full"
 				/>
 			</div>
 			<div>
-				<label for="date" class="font-medium text-base-content block">Date</label>
-				<input
-					type="datetime-local"
-					id="date"
-					name="date"
-					class="input input-bordered w-full focus-visible:outline-base-content border-base-content"
-				/>
+				<label for="date" class="font-medium text-token">Date</label>
+				<input type="datetime-local" id="date" name="date" class="input  w-full" />
 			</div>
 
 			<div>
-				<label for="picture" class="font-medium text-base-content block">Picture</label>
-				<input
-					type="file"
-					accept="image/*"
-					id="picture"
-					name="picture"
-					class="file-input file-input-bordered w-full focus-visible:outline-base-content border-base-content"
-				/>
+				<label for="picture" class="font-medium text-token">Picture</label>
+				<input type="file" accept="image/*" id="picture" name="picture" class="input w-full" />
 			</div>
 			<div>
-				<label for="type" class="font-medium text-base-content block">Type</label>
-				<select
-					name="type"
-					id="type"
-					class="select select-bordered w-full focus-visible:outline-base-content border-base-content"
-				>
+				<label for="type" class="font-medium text-token">Type</label>
+				<select name="type" id="type" class="select w-full">
 					<option disabled selected>Choose event type</option>
 					{#each types as type}
 						<option value={type}>{type}</option>
@@ -89,41 +70,35 @@
 				</select>
 			</div>
 			<div>
-				<label for="category" class="font-medium text-base-content block">Category</label>
-				<select
-					name="category"
-					id="category"
-					class="select select-bordered w-full focus-visible:outline-base-content border-base-content"
-				>
+				<label for="category" class="font-medium text-token">Category</label>
+				<select name="category" id="category" class="select w-full">
 					<option disabled selected>Choose event Category</option>
 					{#each categories as category}
 						<option value={category}>{category}</option>
 					{/each}
 				</select>
 			</div>
-			<div class="md:col-span-2">
-				<label for="description" class="font-medium text-base-content block"
-					>Short Description</label
-				>
-				<input
-					type="text"
-					id="description"
-					name="description"
-					class="input input-bordered w-full focus-visible:outline-base-content border-base-content"
-				/>
-			</div>
-			<div class="md:col-span-2">
-				<label for="longDescription" class="font-medium text-base-content block"
+			<!-- <div class="md:col-span-2">
+				<label for="longDescription" class="font-medium text-token"
 					>Long Description</label
 				>
 				<textarea
 					rows="4"
 					id="longDescription"
 					name="longDescription"
-					class="textarea textarea-bordered w-full resize-none focus-visible:outline-base-content border-base-content"
+					class="textarea textarea-bordered w-full resize-none"
 				/>
-			</div>
+			</div> -->
 		</div>
-		<button type="submit" class="btn md:text-2xl btn-primary normal-case btn-wide">Event</button>
+		<div class="w-full max-w-4xl">
+			<label for="description" class="font-medium text-token">Short Description</label>
+			<textarea rows="3" id="description" name="description" class="textarea w-full" />
+		</div>
+		<Editor />
+		<button
+			type="submit"
+			class="btn md:text-2xl variant-filled-primary md:btn-lg w-full max-w-[19rem] mx-4"
+			>Event</button
+		>
 	</form>
 </section>
